6e746a16206869ee4d4371a52b151cd55009b6e3
Matthew Wire <mjw@mjwconsult.co.uk>
2021-07-21 15:51:53 +0100

    Regenerate civix

--- exportpermission.php
+++ exportpermission.php
@@ -22,8 +22,6 @@ function exportpermission_civicrm_searchTasks($objectName, &$tasks) {
 
 /**
  * Implements hook_civicrm_config().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function exportpermission_civicrm_config(&$config) {
   _exportpermission_civix_civicrm_config($config);
@@ -31,8 +29,6 @@ function exportpermission_civicrm_config(&$config) {
 
 /**
  * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
  */
 function exportpermission_civicrm_xmlMenu(&$files) {
   _exportpermission_civix_civicrm_xmlMenu($files);
@@ -40,8 +36,6 @@ function exportpermission_civicrm_xmlMenu(&$files) {
 
 /**
  * Implements hook_civicrm_install().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function exportpermission_civicrm_install() {
   _exportpermission_civix_civicrm_install();
@@ -49,8 +43,6 @@ function exportpermission_civicrm_install() {
 
 /**
  * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
  */
 function exportpermission_civicrm_postInstall() {
   _exportpermission_civix_civicrm_postInstall();
@@ -58,8 +50,6 @@ function exportpermission_civicrm_postInstall() {
 
 /**
  * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
 function exportpermission_civicrm_uninstall() {
   _exportpermission_civix_civicrm_uninstall();
@@ -67,8 +57,6 @@ function exportpermission_civicrm_uninstall() {
 
 /**
  * Implements hook_civicrm_enable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function exportpermission_civicrm_enable() {
   _exportpermission_civix_civicrm_enable();
@@ -76,8 +64,6 @@ function exportpermission_civicrm_enable() {
 
 /**
  * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
  */
 function exportpermission_civicrm_disable() {
   _exportpermission_civix_civicrm_disable();
@@ -85,8 +71,6 @@ function exportpermission_civicrm_disable() {
 
 /**
  * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
  */
 function exportpermission_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _exportpermission_civix_civicrm_upgrade($op, $queue);
@@ -97,35 +81,15 @@ function exportpermission_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  *
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
  */
 function exportpermission_civicrm_managed(&$entities) {
   _exportpermission_civix_civicrm_managed($entities);
 }
 
 /**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function exportpermission_civicrm_caseTypes(&$caseTypes) {
-  _exportpermission_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
  * Implements hook_civicrm_angularModules().
  *
  * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
  */
 function exportpermission_civicrm_angularModules(&$angularModules) {
   _exportpermission_civix_civicrm_angularModules($angularModules);
@@ -133,8 +97,6 @@ function exportpermission_civicrm_angularModules(&$angularModules) {
 
 /**
  * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
  */
 function exportpermission_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _exportpermission_civix_civicrm_alterSettingsFolders($metaDataFolders);
